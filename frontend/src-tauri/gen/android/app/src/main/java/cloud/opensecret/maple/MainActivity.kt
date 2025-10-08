a4aa94caff1dda5ab86657f17a5bad2d0a8dae57
Tony Giorgio <tonygiorgio@protonmail.com>
2025-10-08 14:36:14 -0500

    Revert "Fix Android status bar overlap with native code solution"
    This reverts commit 8d707a8a682213200248742cbd3e40a4ff9556bb.

--- frontend/src-tauri/gen/android/app/src/main/java/cloud/opensecret/maple/MainActivity.kt
+++ frontend/src-tauri/gen/android/app/src/main/java/cloud/opensecret/maple/MainActivity.kt
@@ -1,14 +1,3 @@
 package cloud.opensecret.maple
 
-import android.os.Bundle
-import androidx.core.view.WindowCompat
-
-class MainActivity : TauriActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Make status bar transparent and allow content to extend under it
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = android.graphics.Color.TRANSPARENT
-    }
-}
+class MainActivity : TauriActivity()
